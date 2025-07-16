92012c1b10c029d6ba29adec6ea3cc372599b474
DatGuyLolzz <alfredolwh@gmail.com>
2025-07-16 11:51:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,21 @@ from flask import Flask, render_template, request, redirect, url_for, flash, jso
 
 app = Flask(__name__)
 
+DB_HOST = os.environ.get('DB_HOST', 'localhost')
+DB_USER = os.environ.get('DB_USER', 'root')
+DB_PASSWORD = os.environ.get('DB_PASSWORD', 'Ilovemysql2025%')
+DB_NAME = os.environ.get('DB_NAME', 'mydb')
+DB_PORT = os.environ.get('DB_PORT', 3306)
+
+try:
+        db_connection = mysql.connector.connect(
+            host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME, port=DB_PORT
+        )
+        cursor = db_connection.cursor(dictionary=True)
+        
+        cursor.execute("SELECT EventID, EventDescription, Date, Time, Venue, Category, ImageFileName FROM event WHERE EventID = %s", (event_id,))
+        event = cursor.fetchone()
+        
 @app.route('/')
 def home():
     return render_template('home.html')
